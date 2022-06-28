e5c8efd71383e9801c7b26e046e62a24ee0b64e1
tRamsay93 <54593330+TaylorRamsay@users.noreply.github.com>
2022-06-28 16:28:10 -0700

    Changed verticalCuts function, deleted unused code
    - verticalCuts function now uses a new image to create processed image rather than pasting onto image one
    - Deleted unused code

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
-from turtle import width
 from PIL import Image, ImageTk
-import os, copy
-
-from numpy import save, typename
+import copy
 
 class ImageManipulator(Frame):
 
@@ -13,9 +9,10 @@ class ImageManipulator(Frame):
     __root.geometry('1200x700')
     __root.title('Image Manipulator')
     __root.config(bg="skyblue")
-    __buttonOneImg = PhotoImage(file=r"C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageOne.png")
-    __buttonTwoImg = PhotoImage(file=r"C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
-    __images = [__buttonOneImg, __buttonTwoImg]
+    __buttonOneImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageOne.png")
+    __buttonTwoImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
+    __processedImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
+    __images = [__buttonOneImg, __buttonTwoImg, __processedImg]
 
     def initFrames(self):
         self.left_frame = Frame(self.__root, width=400, height=690)
@@ -41,6 +38,9 @@ class ImageManipulator(Frame):
         self.openFileTwo = Button(self.left_frame, text="Select Image Two", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: self.addImageTwo(1))
         self.openFileTwo.pack()
 
+        self.processedImg = Label(self.middle_frame, image=self.__images[2])
+        self.processedImg.pack()
+
         self.process = Button(self.middle_frame, text="PROCESS", padx=10, pady=5, fg="white", bg="#263D42", command=self.verticalCuts)
         self.process.pack()
 
@@ -58,9 +58,6 @@ class ImageManipulator(Frame):
         self.imgOne.configure(image=tkimage)
         self.imgOne.image=tkimage
         self.imgOne.pack()
-        #myvar=Label(self.openFile, image = tkimage)
-        #myvar.image = tkimage
-        #myvar.pack()
 
     def addImageTwo(self, k):
         path=filedialog.askopenfilename(filetypes=[("Image File",'.jpg'), ("All Files", "*.*")])
@@ -72,35 +69,42 @@ class ImageManipulator(Frame):
         self.imgTwo.configure(image=tkimage)
         self.imgTwo.image=tkimage
         self.imgTwo.pack()
-        #myvar=Label(self.openFileTwo, image = tkimage)
-        #myvar.image = tkimage
-        #myvar.pack()
+
+    def process(self):
+        resizedImg = self.processedImg.resize((self.__images[0].width, self.__images[0].height))
 
     def verticalCuts(self):
         strip_height = self.__images[0].height
         position = 0
+        self.__images[2] = Image.new(self.__images[0].mode, (self.__images[0].width, self.__images[0].height), color=0)
 
         print(self.__images[0].format, self.__images[0].size, self.__images[0].mode)
         print(self.__images[1].format, self.__images[1].size, self.__images[1].mode)
 
-        for x in range(self.__images[1].width):
-            cut_region = (position, 0, position + 1, strip_height)
-            cut = self.__images[0].crop(cut_region)
-            position += 2
-            self.__images[1].paste(cut, (position, 0))
+        for x in range(self.__images[0].width):
+            if x % 2 == 0:
+                cut_region = (position, 0, position + 1, strip_height)
+                cut = self.__images[0].crop(cut_region)
+                self.__images[2].paste(cut, (position, 0))
+            else:
+                cut_region = (position, 0, position + 1, strip_height)
+                cut = self.__images[1].crop(cut_region)
+                self.__images[2].paste(cut, (position, 0))
+
+            position += 1
 
         print(position)
-        thumb = copy.deepcopy(self.__images[1])
+        thumb = copy.deepcopy(self.__images[2])
         thumb.thumbnail((500, 500))
         tkimage= ImageTk.PhotoImage(thumb)
-        myvar=Label(self.middle_frame, image=tkimage)
-        myvar.image = tkimage
-        myvar.pack()
-        self.__images[1].save("C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\Processed\\test.jpg")
+        self.processedImg.configure(image=tkimage)
+        self.processedImg.image=tkimage
+        self.processedImg.pack()
+        self.__images[2].save("C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\Processed\\test.jpg")
 
     def saveFile(self):
         f = filedialog.asksaveasfile(mode='w', defaultextension='*.jpg*')
-        self.__images[1].save(f)
+        self.__images[2].save(f)
 
     def __init__(self, master=__root):
         Frame.__init__(self, master)
