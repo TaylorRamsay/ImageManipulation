757da01cb9d672bf8c127107d6401e7d89e1cf3b
Taylor Ramsay <54593330+TaylorRamsay@users.noreply.github.com>
2022-06-30 10:43:00 -0700

    GUI Layout Modification
    - Image Thumbnail and buttons now are packed as a grid in the left frame
    - Thumbnail size has been changed

--- main.py
+++ main.py
@@ -6,39 +6,38 @@ import copy
 class ImageManipulator(Frame):
 
     __root = Tk()
-    __root.geometry('1200x700')
+    __root.geometry('1200x800')
     __root.title('Image Manipulator')
     __root.config(bg="skyblue")
-    __buttonOneImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageOne.png")
-    __buttonTwoImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
-    __processedImg = PhotoImage(file=r"C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
+    __buttonOneImg = PhotoImage(file=r"C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageOne.png")
+    __processedImg = PhotoImage(file=r"C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\imageTwo.png")
     __images = [__buttonOneImg, __buttonTwoImg, __processedImg]
 
     def initFrames(self):
-        self.left_frame = Frame(self.__root, width=400, height=690)
+        self.left_frame = Frame(self.__root, width=400, height=790)
         self.left_frame.grid(row=0, column=0, padx=10, pady=5)
         self.left_frame.pack_propagate(False)
         self.left_frame.grid_propagate(False)
 
-        self.middle_frame = Frame(self.__root, width= 750, height=690)
+        self.middle_frame = Frame(self.__root, width= 750, height=790)
         self.middle_frame.grid(row=0, column=1, padx=10, pady=5)
         self.middle_frame.pack_propagate(False)
         self.middle_frame.grid_propagate(False)
 
     def initButtons(self):
         self.imgOne = Label(self.left_frame, image=self.__images[0])
-        self.imgOne.pack()
+        self.imgOne.grid(row=0, column=0, padx=2, pady=5)
 
         self.openFile = Button(self.left_frame, text="Select Image One", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: self.addImageOne(0))
-        self.openFile.pack()
+        self.openFile.grid(row=1, column=0)
 
         self.imgTwo = Label(self.left_frame, image=self.__images[1])
-        self.imgTwo.pack()
+        self.imgTwo.grid(row=2, column=0, padx=2, pady=5)
 
         self.openFileTwo = Button(self.left_frame, text="Select Image Two", padx=10, pady=5, fg="white", bg="#263D42", command=lambda: self.addImageTwo(1))
-        self.openFileTwo.pack()
+        self.openFileTwo.grid(row=3, column=0)
 
-        self.processedImg = Label(self.middle_frame, image=self.__images[2])
+        self.processedImg = Label(self.middle_frame, image=self.__images[2], pady=5)
         self.processedImg.pack()
 
         self.process = Button(self.middle_frame, text="PROCESS", padx=10, pady=5, fg="white", bg="#263D42", command=self.verticalCuts)
@@ -52,23 +51,23 @@ class ImageManipulator(Frame):
         im = Image.open(path)
         cop = copy.deepcopy(im)
         self.__images[k] = cop
-        im.thumbnail((250, 300))
+        im.thumbnail((390, 300))
         tkimage = ImageTk.PhotoImage(im)
         self.openFile.image=im
         self.imgOne.configure(image=tkimage)
         self.imgOne.image=tkimage
-        self.imgOne.pack()
+        self.imgOne.grid(row=0, column=0)
 
     def addImageTwo(self, k):
         path=filedialog.askopenfilename(filetypes=[("Image File",'.jpg'), ("All Files", "*.*")])
         im = Image.open(path)
         cop = copy.deepcopy(im)
         self.__images[k] = cop
-        im.thumbnail((250, 300))
+        im.thumbnail((390, 300))
         tkimage = ImageTk.PhotoImage(im)
         self.imgTwo.configure(image=tkimage)
         self.imgTwo.image=tkimage
-        self.imgTwo.pack()
+        self.imgTwo.grid(row=2, column=0)
 
     def process(self):
         resizedImg = self.processedImg.resize((self.__images[0].width, self.__images[0].height))
@@ -100,7 +99,7 @@ class ImageManipulator(Frame):
         self.processedImg.configure(image=tkimage)
         self.processedImg.image=tkimage
         self.processedImg.pack()
-        self.__images[2].save("C:\\Users\\FSK8475\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\Processed\\test.jpg")
+        self.__images[2].save("C:\\Users\\CDK345\\Documents\\GitHub\\ImageManipulation\\lib\\Icons\\Processed\\test.jpg")
 
     def saveFile(self):
         f = filedialog.asksaveasfile(mode='w', defaultextension='*.jpg*')
